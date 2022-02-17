dd96b2305da144d8ca24f10fd6f8a4c6ae1e0f99
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-02-17 22:36:23 +0400

    task-3. Add method for getting actual worklogs

---
+++ Pet.Jira.Adapter/ActualWorklog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pet.Jira.Adapter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ActualWorklog
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Issue Issue { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TimeSpan TimeSpent { get; set; }
+    }
+}
