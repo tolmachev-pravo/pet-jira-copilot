0e09f38eaef1e6bc1c6b2ee42cd4ef0d63593927
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-04-09 01:00:38 +0400

    task-36. Add mock for jira services

---
+++ src/Pet.Jira.Infrastructure/Mock/MockAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using Pet.Jira.Application.Authentication;
+using System.Threading.Tasks;
+
+namespace Pet.Jira.Infrastructure.Mock
+{
+    internal class MockAuthenticationService : IAuthenticationService
+    {
+        public async Task<LoginResponse> LoginAsync(LoginRequest request)
+        {
+            return new LoginResponse(true);
+        }
+    }
+}
---
+++ src/Pet.Jira.Infrastructure/Mock/MockWorklogStorage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pet.Jira.Domain.Models.Issues;
+using Pet.Jira.Domain.Models.Worklogs;
+
+namespace Pet.Jira.Infrastructure.Mock
+{
+    internal static class MockWorklogStorage
+    {
+        public static IList<Issue> Issues = new List<Issue>
+        {
+            new Issue
+            {
+                Key = "Task-0",
+                Summary = Guid.NewGuid().ToString()
+            },
+            new Issue
+            {
+                Key = "Task-1",
+                Summary = Guid.NewGuid().ToString()
+            },
+            new Issue
+            {
+                Key = "Task-2",
+                Summary = Guid.NewGuid().ToString()
+            },
+            new Issue
+            {
+                Key = "Task-3",
+                Summary = Guid.NewGuid().ToString()
+            },
+            new Issue
+            {
+                Key = "Task-4",
+                Summary = Guid.NewGuid().ToString()
+            }
+        };
+
+        public static IList<IssueWorklog> IssueWorklogs = new List<IssueWorklog>
+        {
+            new IssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddHours(11),
+                ElapsedTime = TimeSpan.FromHours(1),
+                Issue = Issues[0]
+            },
+            new IssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddDays(-1).AddHours(16),
+                ElapsedTime = TimeSpan.FromHours(5),
+                Issue = Issues[1]
+            }
+        };
+
+        public static IList<RawIssueWorklog> RawIssueWorklogs = new List<RawIssueWorklog>
+        {
+            new RawIssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddHours(10),
+                CompletedAt = DateTime.Now.Date.AddHours(11),
+                ElapsedTime = TimeSpan.FromHours(1),
+                Issue = Issues[0]
+            },
+            new RawIssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddHours(11),
+                CompletedAt = DateTime.Now.Date.AddHours(17),
+                ElapsedTime = TimeSpan.FromHours(6),
+                Issue = Issues[1]
+            },
+            new RawIssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddHours(17),
+                CompletedAt = DateTime.Now.Date.AddHours(19),
+                ElapsedTime = TimeSpan.FromHours(2),
+                Issue = Issues[2]
+            },
+            new RawIssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddDays(-1).AddHours(11),
+                CompletedAt = DateTime.Now.Date.AddDays(-1).AddHours(16),
+                ElapsedTime = TimeSpan.FromHours(5),
+                Issue = Issues[1]
+            },
+            new RawIssueWorklog
+            {
+                StartedAt = DateTime.Now.Date.AddDays(-1).AddHours(16),
+                CompletedAt = DateTime.Now.Date.AddDays(-1).AddHours(20),
+                ElapsedTime = TimeSpan.FromHours(4),
+                Issue = Issues[3]
+            }
+        };
+    }
+}
