d63860143fb606736fac54cd4d1700cdc06f9c33
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-04-09 01:00:38 +0400

    task-36. Add mock for jira services

---
+++ src/Pet.Jira.Infrastructure/Mock/MockWorklogDataSource.cs
@@ -0,0 +1,22 @@
+﻿using Pet.Jira.Application.Worklogs;
+using Pet.Jira.Application.Worklogs.Queries;
+using Pet.Jira.Domain.Models.Worklogs;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pet.Jira.Infrastructure.Mock
+{
+    internal class MockWorklogDataSource : IWorklogDataSource
+    {
+        public async Task<IEnumerable<IssueWorklog>> GetIssueWorklogsAsync(GetIssueWorklogs.Query query, CancellationToken cancellationToken = default)
+        {
+            return MockWorklogStorage.IssueWorklogs;
+        }
+
+        public async Task<IEnumerable<RawIssueWorklog>> GetRawIssueWorklogsAsync(GetRawIssueWorklogs.Query query, CancellationToken cancellationToken = default)
+        {
+            return MockWorklogStorage.RawIssueWorklogs;
+        }
+    }
+}
