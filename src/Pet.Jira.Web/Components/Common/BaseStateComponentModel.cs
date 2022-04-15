60c6a1c9028ab0233a35d510b8fe0bb2095e2bc4
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-04-15 18:41:40 +0400

    task-26. Add ILoginStorage

---
+++ src/Pet.Jira.Web/Components/Common/BaseStateComponentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pet.Jira.Web.Components.Common
+{
+    public abstract class BaseStateComponentModel : IStateComponentModel
+    {
+        private ComponentModelState _state = ComponentModelState.Unknown;
+        public ComponentModelState State => _state;
+        public bool InProgress => State == ComponentModelState.InProgress;
+
+        public void StateTo(ComponentModelState state)
+        {
+            _state = state;
+        }
+    }
+}
