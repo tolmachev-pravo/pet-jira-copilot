db0b9e5b39232f66f7091b1765e42a61fad70e43
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-10-20 00:15:22 +0400

    task-101. Don't localize time when adding

--- src/Pet.Jira.Infrastructure/Worklogs/WorklogRepository.cs
+++ src/Pet.Jira.Infrastructure/Worklogs/WorklogRepository.cs
@@ -1,6 +1,4 @@
-﻿using Pet.Jira.Application.Time;
-using Pet.Jira.Application.Users;
-using Pet.Jira.Application.Worklogs;
+﻿using Pet.Jira.Application.Worklogs;
 using Pet.Jira.Application.Worklogs.Dto;
 using Pet.Jira.Infrastructure.Jira;
 using System.Threading;
@@ -11,23 +9,15 @@ namespace Pet.Jira.Infrastructure.Worklogs
     public class WorklogRepository : IWorklogRepository
     {
         private readonly IJiraService _jiraService;
-        private readonly ITimeProvider _timeProvider;
-        private readonly IUserDataSource _userDataSource;
 
         public WorklogRepository(
-            IJiraService jiraService,
-            ITimeProvider timeProvider,
-            IUserDataSource userDataSource)
+            IJiraService jiraService)
         {
             _jiraService = jiraService;
-            _timeProvider = timeProvider;
-            _userDataSource = userDataSource;
         }
 
         public async Task AddAsync(AddedWorklogDto worklog, CancellationToken cancellationToken = default)
         {
-            var user = await _userDataSource.GetCurrentUserAsync(cancellationToken);
-            worklog.StartedAt = _timeProvider.ConvertToServerTimezone(worklog.StartedAt.ToLocalTime(), user.TimeZoneInfo);
             await _jiraService.AddWorklogAsync(worklog);
         }
     }
