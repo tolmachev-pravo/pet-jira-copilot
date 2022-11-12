d13246ea1847de3da832beb1fa12592b04a8d6b6
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-11-12 11:06:44 +0400

    task-123. Update favicon. Fix render MainLayout. Update search image

--- src/Pet.Jira.Web/Components/Common/Stub.razor.cs
+++ src/Pet.Jira.Web/Components/Common/Stub.razor.cs
@@ -7,8 +7,8 @@ namespace Pet.Jira.Web.Components.Common
     public partial class Stub : ComponentBase
     {
         [Parameter] public Color Color { get; set; } = Color.Default;
-        [Parameter] public string Icon { get; set; } = WebConstants.Icons.MonsterAnimal;
+        [Parameter] public string Icon { get; set; } = WebConstants.Icons.Favicon;
         [Parameter] public string Message { get; set; }
-        [Parameter] public string ViewBox { get; set; } = "0 0 570.547 570.547";
+        [Parameter] public string ViewBox { get; set; } = "0 0 260 260";
     }
 }
