1cc9a73048f374f0b2a844ecf37be093494f0885
tolmachev-pravo <d.tolmachev@pravo.tech>
2022-11-12 12:35:58 +0400

    task-126. Try catch clipboard actions

--- src/Pet.Jira.Web/Components/Worklogs/WorklogMenu.razor.cs
+++ src/Pet.Jira.Web/Components/Worklogs/WorklogMenu.razor.cs
@@ -2,6 +2,8 @@
 using MudBlazor;
 using Pet.Jira.Application.Worklogs.Dto;
 using Pet.Jira.Web.Components.Clipboard;
+using Pet.Jira.Web.Shared;
+using System;
 using System.Threading.Tasks;
 
 namespace Pet.Jira.Web.Components.Worklogs
@@ -12,28 +14,37 @@ namespace Pet.Jira.Web.Components.Worklogs
         [Parameter] public Color Color { get; set; } = Color.Default;
         [Parameter] public string Icon { get; set; } = Icons.Outlined.ArrowCircleDown;
 
+        [CascadingParameter] public ErrorHandler ErrorHandler { get; set; }
+
         [Inject] private IClipboard _clipboard { get; set; }
         [Inject] private ISnackbar _snackbar { get; set; }
 
-        private async Task CopyToClipboardInReviewAsync(WorklogCollectionItem entity)
+        private async Task CopyToClipboardInReviewAsync()
         {
-            var text = $"**IN REVIEW** [{entity.Issue.Key}]({entity.Issue.Link}) {entity.Issue.Summary}";
+            var text = $"**IN REVIEW** [{Entity.Issue.Key}]({Entity.Issue.Link}) {Entity.Issue.Summary}";
             await CopyToClipboardAsync(text);
         }
 
-        private async Task CopyToClipboardInProgressAsync(WorklogCollectionItem entity)
+        private async Task CopyToClipboardInProgressAsync()
         {
-            var text = $"**IN PROGRESS** [{entity.Issue.Key}]({entity.Issue.Link}) {entity.Issue.Summary}";
+            var text = $"**IN PROGRESS** [{Entity.Issue.Key}]({Entity.Issue.Link}) {Entity.Issue.Summary}";
             await CopyToClipboardAsync(text);
         }
 
         private async Task CopyToClipboardAsync(string text)
         {
-            await _clipboard.CopyToAsync(text);
-            _snackbar.Add(
-                $"Copied to clipboard",
-                Severity.Success,
-                config => { config.ActionColor = Color.Success; });
+            try
+            {
+                await _clipboard.CopyToAsync(text);
+                _snackbar.Add(
+                    $"Copied to clipboard",
+                    Severity.Success,
+                    config => { config.ActionColor = Color.Success; });
+            }
+            catch (Exception e)
+            {
+                ErrorHandler.ProcessError(e);
+            }
         }
     }
 }
