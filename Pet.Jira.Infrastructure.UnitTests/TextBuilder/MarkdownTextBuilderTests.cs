88af805b834f34fdb1122831a1ea86cce91eb465
tolmachev-pravo <d.tolmachev@pravo.tech>
2023-01-03 09:46:45 +0400

    task-53. Add unit tests

---
+++ Pet.Jira.Infrastructure.UnitTests/TextBuilder/MarkdownTextBuilderTests.cs
@@ -0,0 +1,72 @@
+﻿using Pet.Jira.Application.TextBuilder;
+using Pet.Jira.Infrastructure.TextBuilder;
+
+namespace Pet.Jira.Infrastructure.UnitTests.TextBuilder
+{
+    [TestFixture]
+    public class MarkdownTextBuilderTests
+    {
+		private MarkdownTextBuilder _textBuilder;
+
+        [SetUp]
+		public void SetUp()
+		{
+            _textBuilder = new MarkdownTextBuilder();
+        }
+
+        [TestCase("https://localhost.ru", "site url", $"[site url](https://localhost.ru) ")]
+        [TestCase(null, "site url", $"[site url]() ")]
+        [TestCase("https://localhost.ru", null, $"[](https://localhost.ru) ")]
+        [TestCase(null, null, $"[]() ")]
+        public void AddLink_Should_BeCorrect(string href, string value, string expected)
+		{
+            // Arrange
+            // Act
+            _textBuilder.AddLink(href, value);
+
+            // Assert
+            var result = _textBuilder.Build();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("**text** ", "text", TextOption.Bold)]
+        [TestCase("****text**** ", "text", TextOption.Bold, TextOption.Bold)]
+        [TestCase("text ", "text")]
+        [TestCase(" ", "")]
+        [TestCase(" ", null)]
+        public void AddText_Should_BeCorrect(string expected, string text, params TextOption[] options)
+        {
+            // Arrange
+            // Act
+            _textBuilder.AddText(text, options);
+
+            // Assert
+            var result = _textBuilder.Build();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("**", TextOption.Bold)]
+        public void BeginOption_Should_BeCorrect(string expected, TextOption option)
+        {
+            // Arrange
+            // Act
+            _textBuilder.BeginOption(option);
+
+            // Assert
+            var result = _textBuilder.Build();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("**", TextOption.Bold)]
+        public void EndOption_Should_BeCorrect(string expected, TextOption option)
+        {
+            // Arrange
+            // Act
+            _textBuilder.EndOption(option);
+
+            // Assert
+            var result = _textBuilder.Build();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
