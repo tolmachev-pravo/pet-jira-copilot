9e2095d7570e2099d430870781231abdc1e53d58
tolmachev-pravo <d.tolmachev@pravo.tech>
2026-06-06 07:35:27 +0400

    test: add unit tests for DeleteArticleCommandHandler

---
+++ tests/Pet.Jira.UnitTests/Application/Articles/Commands/DeleteArticleCommandHandlerTests.cs
@@ -0,0 +1,56 @@
+using Moq;
+using Pet.Jira.Application.Articles;
+using Pet.Jira.Application.Articles.Commands.DeleteArticle;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pet.Jira.UnitTests.Application.Articles.Commands
+{
+    [TestFixture]
+    public class DeleteArticleCommandHandlerTests
+    {
+        private DeleteArticleCommandHandler _handler;
+        private Mock<IArticleRepository> _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = new Mock<IArticleRepository>();
+            _handler = new DeleteArticleCommandHandler(_repository.Object);
+        }
+
+        [Test]
+        public async Task Handle_Should_ReturnTrue_When_ArticleExists()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _repository
+                .Setup(r => r.DeleteAsync(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _handler.Handle(new DeleteArticleCommand { Id = id }, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.True);
+            _repository.Verify(r => r.DeleteAsync(id, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public async Task Handle_Should_ReturnFalse_When_ArticleNotFound()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _repository
+                .Setup(r => r.DeleteAsync(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _handler.Handle(new DeleteArticleCommand { Id = id }, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
