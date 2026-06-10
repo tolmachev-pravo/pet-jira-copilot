8c4b9604441f4ea909e86fbac7410ba95014ca51
tolmachev-pravo <d.tolmachev@pravo.tech>
2026-06-10 11:54:01 +0400

    feat: add Event record and EventSource enum

---
+++ src/Pet.Jira.Domain/Models/Events/Event.cs
@@ -0,0 +1,15 @@
+using Pet.Jira.Domain.Models.Issues;
+using System;
+
+namespace Pet.Jira.Domain.Models.Events
+{
+    public record Event(
+        DateTime Start,
+        DateTime End,
+        string Title,
+        string? Description,
+        Uri? Link,
+        Issue? Issue,
+        EventSource Source
+    );
+}
